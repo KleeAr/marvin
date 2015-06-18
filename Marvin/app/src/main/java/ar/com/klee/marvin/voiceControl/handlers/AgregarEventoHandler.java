c20915e880224b810602bf3e0d8911476b643fe0
fedesinopoli <federico.sinopoli@gmail.com>
2015-06-17 22:27:10 -0300

    Avance Calendar y Mail
    Avance en la integración de calendar y mail por voz

--- Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/AgregarEventoHandler.java
+++ Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/AgregarEventoHandler.java
@@ -2,8 +2,10 @@ package ar.com.klee.marvin.voiceControl.handlers;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 import ar.com.klee.marvin.expressions.ExpressionMatcher;
@@ -13,11 +15,16 @@ import ar.com.klee.marvin.voiceControl.TTS;
 
 public class AgregarEventoHandler extends CommandHandler{
 
+    private static final String TIME24HOURS_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+
     private ExpressionMatcher expressionMatcher;
     private String command;
     private String event;
-    private String day;
-    private String hour;
+    private int day;
+    private int month;
+    private int year;
+    private int hour;
+    private int minute;
     private boolean setEvent = false;
     private TTS textToSpeech;
 
@@ -108,13 +115,67 @@ public class AgregarEventoHandler extends CommandHandler{
     //INDICA FECHA
     public int stepFive(String input){
 
-        /*
-        VALIDAR FECHA
-         */
+        ExpressionMatcher dateFormat = new ExpressionMatcher("{day} de {month} del {year}");
+
+        if(!dateFormat.matches(input)){
+
+            textToSpeech.speakText("Debe indicar la fecha con el formato: 13 de abril del 2016");
+
+            return 5;
+
+        }
+
+        Map<String, String> values = dateFormat.getValuesFromExpression(input);
+
+        String dayStr, monthStr, yearStr;
+
+        dayStr = values.get("day");
+        monthStr = values.get("month");
+        yearStr = values.get("year");
+
+        try{
+
+            day = Integer.parseInt(dayStr);
+
+        }catch(NumberFormatException e){
+
+            textToSpeech.speakText("Debe indicar un número para el día");
+
+            return 5;
+
+        }
+
+        try{
+
+            year = Integer.parseInt(yearStr);
+
+        }catch(NumberFormatException e){
+
+            textToSpeech.speakText("Debe indicar un número para el año");
+
+            return 5;
+
+        }
+
+        month = numericMonth(monthStr);
+
+        if(month == 0){
+
+            textToSpeech.speakText("Debe indicar la fecha con el formato: 13 de abril del 2016");
+
+            return 5;
+
+        }
+
+        if(!validateDate(day, month, year)){
+
+            textToSpeech.speakText("La fecha indicada es inválida");
+
+            return 5;
+
+        }
 
         textToSpeech.speakText("¿El evento es el "+input+"?");
-
-        day = input;
 
         return 7;
     }
@@ -146,9 +207,56 @@ public class AgregarEventoHandler extends CommandHandler{
     //INGRESA HORA
     public int stepNine(String input){
 
-        textToSpeech.speakText("¿El evento es las "+input+"?");
+        ExpressionMatcher hourFormat = new ExpressionMatcher("{hour} y {minute}");
 
-        hour = input;
+        if(!hourFormat.matches(input)){
+
+            textToSpeech.speakText("Debe indicar la hora como el siguiente ejemplo: 22 y 15");
+
+            return 9;
+
+        }
+
+        Map<String, String> values = hourFormat.getValuesFromExpression(input);
+
+        String hourStr, minuteStr;
+
+        hourStr = values.get("hour");
+        minuteStr = values.get("minute");
+
+        try{
+
+            hour = Integer.parseInt(hourStr);
+
+        }catch(NumberFormatException e){
+
+            textToSpeech.speakText("Debe indicar un número para la hora");
+
+            return 9;
+
+        }
+
+        try{
+
+            minute = Integer.parseInt(minuteStr);
+
+        }catch(NumberFormatException e){
+
+            textToSpeech.speakText("Debe indicar un número para el minuto");
+
+            return 9;
+
+        }
+
+        if(!validateHour(hour,minute)){
+
+            textToSpeech.speakText("Indicaste una hora incorrecta");
+
+            return 9;
+
+        }
+
+        textToSpeech.speakText("¿El evento es las "+input+"?");
 
         return 11;
 
@@ -159,6 +267,7 @@ public class AgregarEventoHandler extends CommandHandler{
 
         if(input.equals("si")) {
             textToSpeech.speakText("Agregando evento en el calendario");
+            createGoogleCalendarEvent();
             return 0;
         }
 
@@ -178,9 +287,79 @@ public class AgregarEventoHandler extends CommandHandler{
 
     }
 
-    public void createGoogleCalendarEvent(int year, int month, int day){
+    public void createGoogleCalendarEvent(){
+
+        Log.d("MVN_", event + " " + year + "/" + month + "/" + day + " - " + hour + ":" + minute);
 
         calendarService.createEvent(year, month, day);
+
+    }
+
+    public int numericMonth(String monthStr){
+
+        monthStr = monthStr.toLowerCase();
+
+        switch(monthStr){
+
+            case "enero":
+                return 1;
+            case "febrero":
+                return 2;
+            case "marzo":
+                return 3;
+            case "abril":
+                return 4;
+            case "mayo":
+                return 5;
+            case "junio":
+                return 6;
+            case "julio":
+                return 7;
+            case "agosto":
+                return 8;
+            case "septiembre":
+                return 9;
+            case "setiembre":
+                return 9;
+            case "octubre":
+                return 10;
+            case "noviembre":
+                return 11;
+            case "diciembre":
+                return 12;
+
+        }
+
+        return 0;
+    }
+
+    public boolean validateDate(int inputDay, int inputMonth, int inputYear){
+
+        try {
+            GregorianCalendar gc = new GregorianCalendar();
+            gc.setLenient(false);        // must do this
+            gc.set(GregorianCalendar.YEAR, inputYear);
+            gc.set(GregorianCalendar.MONTH, inputMonth);
+            gc.set(GregorianCalendar.DATE, inputDay);
+
+            gc.getTime(); // exception thrown here
+
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+            return false;
+        }
+
+    }
+
+    public boolean validateHour(int hour, int minute){
+
+        if(minute>=0 && minute<=59 && hour>=0 && hour<=23)
+            return true;
+
+        return false;
 
     }
 }
--- Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/EnviarMailAContactoHandler.java
+++ Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/EnviarMailAContactoHandler.java
@@ -1,6 +1,8 @@
 package ar.com.klee.marvin.voiceControl.handlers;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 
 import java.util.Map;
 
@@ -13,15 +15,20 @@ public class EnviarMailAContactoHandler extends CommandHandler{
     private ExpressionMatcher expressionMatcher;
     private String command;
     private String contact;
+    private String message;
+    private String subject = "";
     private boolean setContact = false;
     private TTS textToSpeech;
+    private Context context;
 
-    public EnviarMailAContactoHandler(String command, TTS textToSpeech){
+    public EnviarMailAContactoHandler(String command, TTS textToSpeech, Context context){
 
         expressionMatcher = new ExpressionMatcher("enviar mail a {contacto}");
 
         this.command = command;
         this.textToSpeech = textToSpeech;
+
+        this.context = context;
 
     }
 
@@ -50,6 +57,12 @@ public class EnviarMailAContactoHandler extends CommandHandler{
                 return stepFive(input);
             case 7:
                 return stepSeven(input);
+            case 9:
+                return stepNine(input);
+            case 11:
+                return stepEleven(input);
+            case 13:
+                return stepThirteen(input);
 
         }
 
@@ -59,6 +72,10 @@ public class EnviarMailAContactoHandler extends CommandHandler{
 
     //PRONUNCIA CONTACTO
     public int stepOne(){
+
+        //OBTENER MAIL DEL CONTACTO
+
+        //CONTACTO INEXISTENTE - CONTACTO SIN MAIL
 
         textToSpeech.speakText("¿Querés enviar un mail al contacto " + contact + "?");
 
@@ -98,6 +115,8 @@ public class EnviarMailAContactoHandler extends CommandHandler{
 
         textToSpeech.speakText("¿Querés enviar por mail el mensaje " + input + "?");
 
+        message = input;
+
         return 7;
 
     }
@@ -106,8 +125,9 @@ public class EnviarMailAContactoHandler extends CommandHandler{
     public int stepSeven(String input){
 
         if(input.equals("si")) {
-            textToSpeech.speakText("Enviando mail");
-            return 0;
+            textToSpeech.speakText("¿Deseás agregar un asunto?");
+
+            return 9;
         }
 
         if(input.equals("cancelar")) {
@@ -123,6 +143,83 @@ public class EnviarMailAContactoHandler extends CommandHandler{
         textToSpeech.speakText("Debe indicar sí, no o cancelar");
 
         return 7;
+
+    }
+
+    //INDICA SI QUIERE AGREGAR ASUNTO
+    public int stepNine(String input){
+
+        if(input.equals("si")) {
+            textToSpeech.speakText("¿Qué asunto deseás agregar?");
+            return 11;
+        }
+
+        if(input.equals("cancelar")) {
+            textToSpeech.speakText("Cancelando envío");
+            return 0;
+        }
+
+        if(input.equals("no")){
+            textToSpeech.speakText("Enviando mail");
+            sendMail();
+            return 0;
+        }
+
+        textToSpeech.speakText("Debe indicar sí, no o cancelar");
+
+        return 9;
+
+    }
+
+    //INDICA ASUNTO
+    public int stepEleven(String input){
+
+        textToSpeech.speakText("¿Querés enviar el asunto " + input + "?");
+
+        subject = input;
+
+        return 13;
+
+    }
+
+    //CONFIRMA ASUNTO
+    public int stepThirteen(String input){
+
+        if(input.equals("si")) {
+            textToSpeech.speakText("Enviando mail");
+            sendMail();
+            return 0;
+        }
+
+        if(input.equals("cancelar")) {
+            textToSpeech.speakText("Cancelando envío");
+            return 0;
+        }
+
+        if(input.equals("no")){
+            textToSpeech.speakText("¿Qué asunto deseás agregar?");
+            return 11;
+        }
+
+        textToSpeech.speakText("Debe indicar sí, no o cancelar");
+
+        return 13;
+
+    }
+
+
+    public void sendMail(){
+
+        message = message + "\n\n\n" + "Mensaje enviado a través de MARVIN";
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, contact);
+        //emailIntent.putExtra(Intent.EXTRA_CC, cc);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(Intent.createChooser(emailIntent, "Seleccionar cuenta de Email:"));
 
     }
 
