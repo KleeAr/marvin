9d17915d6dcd18f40f3804d0d4e487fea763e764
fedesinopoli <federico.sinopoli@gmail.com>
2015-06-28 22:45:59 -0300

    Abrir aplicaciones por voz

--- Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/AbrirAplicacionHandler.java
+++ Marvin/app/src/main/java/ar/com/klee/marvin/voiceControl/handlers/AbrirAplicacionHandler.java
@@ -2,7 +2,11 @@ package ar.com.klee.marvin.voiceControl.handlers;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
+import java.util.List;
 import java.util.Map;
 
 import ar.com.klee.marvin.activities.CameraActivity;
@@ -22,15 +26,39 @@ public class AbrirAplicacionHandler extends CommandHandler{
 
         String app = values.get("aplicacion");
 
-        getTextToSpeech().speakText("Abriendo "+ app);
-        
-        //CODIGO PARA ABRIR APLICACION
-
         if(app.equals("cámara")) {
             Intent intent = new Intent(getContext(), CameraActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getTextToSpeech().speakText("Abriendo "+ app);
             getContext().startActivity(intent);
+            currentContext.put(STEP, 0);
+            return currentContext;
+        }else if(app.equals("marvin")) {
+            getTextToSpeech().speakText("La aplicación " + app + " ya se está ejecutando");
+            currentContext.put(STEP, 0);
+            return currentContext;
+        }else{
+            final PackageManager pm = getContext().getPackageManager();
+            //get a list of installed apps.
+            List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+            int i = 0;
+
+            while(i < packages.size()) {
+                String appLabel = pm.getApplicationLabel(packages.get(i)).toString();
+
+                if(appLabel.toLowerCase().equals(app)){
+                    getTextToSpeech().speakText("Abriendo "+ app);
+                    getCommandHandlerManager().getMainActivity().startActivity(pm.getLaunchIntentForPackage(packages.get(i).packageName));
+                    currentContext.put(STEP, 0);
+                    return currentContext;
+                }
+
+                i++;
+            }
         }
+
+        getTextToSpeech().speakText("La aplicación " + app + " no se encuentra instalada.");
         currentContext.put(STEP, 0);
         return currentContext;
 
