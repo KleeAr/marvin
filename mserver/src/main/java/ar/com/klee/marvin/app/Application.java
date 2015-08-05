e73a4312d37bd7f87bb8ceb920146fd1d6233430
matiasdsalerno <matias.d.salerno@gmail.com>
2015-08-04 22:04:35 -0300

    Server user y user setting full stack con base de datos en memoria

--- mserver/src/main/java/ar/com/klee/marvin/app/Application.java
+++ mserver/src/main/java/ar/com/klee/marvin/app/Application.java
@@ -2,10 +2,14 @@ package ar.com.klee.marvin.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"ar.com.klee.marvin.*"})
+@EnableJpaRepositories({"ar.com.klee.marvin.repository"})
+@EntityScan({"ar.com.klee.marvin.model"})
 public class Application {
 
 	public static void main(String[] args) {
--- mserver/src/main/java/ar/com/klee/marvin/controller/UserController.java
+++ mserver/src/main/java/ar/com/klee/marvin/controller/UserController.java
@@ -1,16 +1,50 @@
 package ar.com.klee.marvin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.klee.marvin.model.User;
+import ar.com.klee.marvin.repository.UserRepository;
 
 @RestController
+@RequestMapping(value = "/users")
 public class UserController {
 
-	@RequestMapping(method = RequestMethod.GET,value = "/users")
-	public User getUsers() {
-		return new User(1L, "Matías", "Salerno", "matias.d.salerno@gmail.com");
+	private UserRepository userRepository;
+	
+	@Autowired	
+	public UserController(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Iterable<User> getAll() {
+		return userRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public User getById(@PathVariable("id")Long id) {
+		return userRepository.findOne(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public User save(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public User update(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id")Long id) {
+		userRepository.delete(id);
+	}
+	
 }
